6f79332a1308048e5896aecee97dad23d596b4c1
Private-053 <hugojbrandao@gmail.com>
2022-01-03 04:11:57 +0000

    stuffs

--- src/server/DataBase.java
+++ src/server/DataBase.java
@@ -2,19 +2,18 @@ package server;
 
 import common.Flight;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DataBase {
 
     private Map<String, Flight> flights;
     private Map<String, Client> clients;
+    private Map<String, List<Flight>> stopOver;
 
     public DataBase(){
         this.flights = new HashMap<>();
         this.clients = new HashMap<>();
+        this.stopOver = new HashMap<>();
     }
 
     public void addFligth(String i, String o, String d, int c, int occupation){
@@ -37,5 +36,33 @@ public class DataBase {
 
     public boolean checkDate(Flight f1, Flight f2){
         return f1.getDate().equals(f2.getDate());
+    }
+
+    public void addStopOver(Flight f){
+        List<Flight> aux = new ArrayList<>();
+        for(Flight flight : flights.values()){
+            if(checkDate(f,flight)) stopOver.put(randomString(), getSameOrigin(f.getDestination()));
+        }
+    }
+
+    public String getStopOverOrigin(String id){
+        return stopOver.get(id).get(0).getOrigin();
+    }
+
+    public String getStopOverDestination(String id){
+        int length = stopOver.get(id).size();
+        return stopOver.get(id).get(length-1).getDestination();
+    }
+
+    public String randomString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
