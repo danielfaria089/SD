dd6a817e64ee19e7bf70c966c1bd250f26b295c1
Private-053 <hugojbrandao@gmail.com>
2022-01-12 15:39:46 +0000

    update writeBookings(acho que ta pronto para testes)

--- src/server/ColBookings.java
+++ src/server/ColBookings.java
@@ -216,7 +216,7 @@ public class ColBookings {
         }
     }
 
-    public void writeFlights(String filename) throws IOException {
+    public void writeBookings(String filename) throws IOException {
         PrintWriter writer = new PrintWriter((new FileWriter(filename)));
         for(Booking b : reservations.values()){
             List<Flight> aux = new ArrayList<>(b.getStopOvers().getStopOvers());
